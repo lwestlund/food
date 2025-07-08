028a18138b3c79973950f16d24a572377d02aaf0
Love Westlund <love.westlund@proton.me>
2025-07-08 21:16:12 +0200

    feat: add a web server to the frontend
    This makes it possible to not only printing some markup to stdout, but
    actually respond to requests! 🥳

--- food-frontend/src/main.rs
+++ food-frontend/src/main.rs
@@ -1,9 +1,66 @@
-use maud::html;
+use axum::{
+    Router,
+    extract::FromRequestParts,
+    response::{IntoResponse, Response},
+    routing::get,
+};
+use maud::{DOCTYPE, Markup, html};
 
 #[tokio::main]
-async fn main() {
-    let markup = html! {
+async fn main() -> anyhow::Result<()> {
+    let port = std::env::var("PORT").unwrap_or_else(|_| "3002".to_string());
+
+    let app = Router::new().route("/", get(hello_world));
+    let address = format!("0.0.0.0:{port}");
+    let listener = tokio::net::TcpListener::bind(address.clone()).await?;
+
+    println!("Serving at http://{address}/");
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+async fn hello_world(layout: Layout) -> Response {
+    let body = html! {
+        h1 { "Food" }
         p { "Hello, world!" }
     };
-    println!("{}", markup.into_string());
+    layout.render(&body)
+}
+
+struct Layout {
+    title: String,
+}
+
+impl Layout {
+    fn render(self, body: &Markup) -> Response {
+        html! {
+            (DOCTYPE)
+            html {
+                head {
+                    meta charset="utf-8";
+                    title { (self.title) }
+                }
+                body {
+                    (body)
+                }
+            }
+        }
+        .into_response()
+    }
+}
+
+impl<S> FromRequestParts<S> for Layout
+where
+    S: Send + Sync,
+{
+    type Rejection = std::convert::Infallible;
+
+    async fn from_request_parts(
+        _parts: &mut axum::http::request::Parts,
+        _state: &S,
+    ) -> Result<Self, Self::Rejection> {
+        let title = "Food".to_owned();
+        Ok(Self { title })
+    }
 }
