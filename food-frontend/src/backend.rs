d71935907bb15efa7cd9ce51991583acda90cc2d
Love Westlund <love.westlund@proton.me>
2025-07-15 11:10:52 +0200

    feat: recipes from backend in the frontend (and errors)
    This uses dynamic routing to generate routes for recipes on the fly.

--- food-frontend/src/backend.rs
+++ food-frontend/src/backend.rs
@@ -1,6 +1,6 @@
 use std::net::IpAddr;
 
-use anyhow::bail;
+use axum::body::Bytes;
 use reqwest::header;
 
 use crate::mime;
@@ -28,24 +28,58 @@ impl Backend {
         )
     }
 
-    pub(crate) async fn get_recipe_listing(&self) -> anyhow::Result<Vec<models::RecipeListing>> {
-        let url = self.format_url("/api/recipes");
+    async fn get_json<T>(&self, endpoint: &str) -> Result<T, BackendError>
+    where
+        for<'de> T: serde::Deserialize<'de>,
+    {
+        let bytes = self.get_json_bytes(endpoint).await?;
+        let t: T = serde_json::from_slice(&bytes)?;
+        Ok(t)
+    }
+    async fn get_json_bytes(&self, endpoint: &str) -> Result<Bytes, BackendError> {
+        let url = self.format_url(endpoint);
 
         let response = self
             .client
             .get(url)
             .header(header::ACCEPT, mime::APPLICATION_JSON)
             .send()
-            .await?;
+            .await?
+            .error_for_status()?;
         let Some(content_type) = response.headers().get(header::CONTENT_TYPE) else {
-            bail!("response does not have a content type");
+            return Err(BackendError::UnknownContentType(None));
         };
         if content_type != mime::APPLICATION_JSON {
-            bail!("Did not get json back");
+            let content_type = String::from_utf8_lossy(content_type.as_bytes()).into_owned();
+            return Err(BackendError::UnknownContentType(Some(content_type)));
         }
         let bytes = response.bytes().await?;
+        Ok(bytes)
+    }
 
-        let recipe_listing = serde_json::from_slice(&bytes)?;
-        Ok(recipe_listing)
+    pub(crate) async fn get_recipe_listing(
+        &self,
+    ) -> Result<Vec<models::RecipeListing>, BackendError> {
+        self.get_json("/api/recipes").await
+    }
+
+    pub(crate) async fn get_recipe_by_id(&self, id: i64) -> Result<models::Recipe, BackendError> {
+        self.get_json(&format!("/api/recipes/{id}")).await
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub(crate) enum BackendError {
+    #[error("error in request to backend: {0}")]
+    Reqwest(#[from] reqwest::Error),
+    #[error("unknown content type in response from backend: {0:?}")]
+    UnknownContentType(Option<String>),
+    #[error("error deserializing response as json: {0}")]
+    Json(#[from] serde_json::Error),
+}
+
+impl From<Option<String>> for BackendError {
+    fn from(value: Option<String>) -> Self {
+        Self::UnknownContentType(value)
     }
 }
--- food-frontend/src/main.rs
+++ food-frontend/src/main.rs
@@ -1,15 +1,17 @@
 mod backend;
+mod error;
 mod mime;
 
 use backend::Backend;
+use error::{AppError, ErrorResponse, ErrorWithLayout as _, ResultWithLayout as _};
+use heck::ToKebabCase;
 
 use std::net::{IpAddr, Ipv4Addr};
 
 use anyhow::Context;
 use axum::{
     Router,
-    extract::{FromRequestParts, State},
-    response::{IntoResponse, Response},
+    extract::{FromRequestParts, Path, State},
     routing::get,
 };
 use maud::{DOCTYPE, Markup, html};
@@ -35,6 +37,8 @@ async fn main() -> anyhow::Result<()> {
     let port = std::env::var("PORT").unwrap_or_else(|_| "3002".to_string());
     let app = Router::new()
         .route("/", get(root))
+        .route("/recipes/{recipe}", get(recipe))
+        .fallback(handler_404)
         .with_state(backend)
         .layer(LiveReloadLayer::new());
     let address = format!("0.0.0.0:{port}");
@@ -46,32 +50,106 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-async fn root(State(backend): State<Backend>, layout: Layout) -> Response {
-    let recipe_listing = backend.get_recipe_listing().await.unwrap();
+async fn root(State(backend): State<Backend>, layout: Layout) -> Result<Markup, ErrorResponse> {
+    let recipe_listing = backend
+        .get_recipe_listing()
+        .await
+        .map_err(Into::into)
+        .with_layout(&layout)?;
     let listing = recipe_listing.into_iter().map(|listing| {
+        let slug = recipe_id_title_to_slug(listing.id, &listing.title);
+        let path = format!("/recipes/{slug}");
         html! {
             li {
-                (listing.title)
+                a href=(path){
+                    (listing.title)
+                }
             }
         }
     });
     let body = html! {
-        h1 { "Food" }
+        h1 { "Welcome to my recipes" }
+        p { "We've got:" }
         ul {
             @for listing in listing {
                 (listing)
             }
         }
     };
-    layout.render(&body)
+    Ok(layout.render(&body))
 }
 
+async fn recipe(
+    State(backend): State<Backend>,
+    layout: Layout,
+    Path(recipe_slug): Path<String>,
+) -> Result<Markup, ErrorResponse> {
+    let (id, _title) = recipe_slug
+        .split_once('-')
+        .ok_or(AppError::NotFound.with_layout(&layout))?;
+    let id: i64 = id
+        .parse()
+        .map_err(|_| AppError::NotFound.with_layout(&layout))?;
+
+    let recipe = backend
+        .get_recipe_by_id(id)
+        .await
+        .map_err(Into::into)
+        .with_layout(&layout)?;
+
+    let expected_slug = recipe_id_title_to_slug(id, &recipe.title);
+    if expected_slug != recipe_slug {
+        return Err(AppError::NotFound.with_layout(&layout));
+    }
+
+    let body = html! {
+        h2 { (recipe.title) }
+        p { (recipe.description) }
+        p { (recipe.meal_type) }
+
+        div {
+            ul {
+                @for ingredient in recipe.ingredients {
+                    li { (ingredient.quantity) " " (ingredient.unit) " " (ingredient.name) }
+                }
+            }
+        }
+
+        div {
+            ol {
+                @for instruction in recipe.instructions {
+                    li { (instruction) }
+                }
+            }
+        }
+
+        @if let Some(source_url) = recipe.source_url {
+            p { (recipe.source_name) ", " (source_url) }
+        } @else {
+            p { (recipe.source_name) }
+        }
+
+        p { (recipe.creation_date) }
+    };
+
+    Ok(layout.render(&body))
+}
+
+fn recipe_id_title_to_slug(id: i64, title: &str) -> String {
+    format!("{}-{}", id, title.to_kebab_case())
+}
+
+async fn handler_404(layout: Layout) -> ErrorResponse {
+    AppError::NotFound.with_layout(&layout)
+}
+
+#[derive(Clone)]
 struct Layout {
     title: String,
 }
 
 impl Layout {
-    fn render(self, body: &Markup) -> Response {
+    fn render(self, body: &Markup) -> Markup {
         html! {
             (DOCTYPE)
             html {
@@ -80,11 +158,21 @@ impl Layout {
                     title { (self.title) }
                 }
                 body {
+                    nav {
+                        a href="/" { "Food" }
+                    }
                     (body)
                 }
             }
         }
-        .into_response()
+    }
+
+    fn with_status(mut self, status: &str) -> Self {
+        const SEPARATOR: &str = " | ";
+        self.title.reserve(status.len() + SEPARATOR.len());
+        self.title.insert_str(0, SEPARATOR);
+        self.title.insert_str(0, status);
+        self
     }
 }
 
@@ -97,7 +185,7 @@ where
     async fn from_request_parts(
         _parts: &mut axum::http::request::Parts,
         _state: &S,
-    ) -> Result<Self, Self::Rejection> {
+    ) -> std::result::Result<Self, Self::Rejection> {
         let title = "Food".to_owned();
         Ok(Self { title })
     }
